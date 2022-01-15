df95af6c315c0fd3f363b940290853a3a0f93362
tianyuan <tianyuan@huayun.com>
2022-01-15 15:49:20 +0800

    实现优雅重启

--- gin-blog/cmd/main.go
+++ gin-blog/cmd/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"gin-blog/pkg/logging"
 	"gin-blog/pkg/setting"
 	"gin-blog/routers"
-	"net/http"
+	"github.com/fvbock/endless"
+	"syscall"
 )
 
 func main() {
@@ -17,15 +19,44 @@ func main() {
 	//})
 	//router.Run()
 
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr: fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler: router,
-		ReadTimeout: setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		logging.Info("Actual pid is %d", syscall.Getpid())
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		logging.Info("Server err: %v", err)
 	}
 
-	s.ListenAndServe()
+	//router := routers.InitRouter()
+	//
+	//s := &http.Server{
+	//	Addr: fmt.Sprintf(":%d", setting.HTTPPort),
+	//	Handler: router,
+	//	ReadTimeout: setting.ReadTimeout,
+	//	WriteTimeout: setting.WriteTimeout,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+
+	////s.ListenAndServe()
+	//go func() {
+	//	if err := s.ListenAndServe(); err != nil {
+	//		log.Printf("Listen: %s\n", err)
+	//	}
+	//}()
+	//quit := make(chan os.Signal)
+	//signal.Notify(quit, os.Interrupt)
+	//<- quit
+	//log.Println("Shutdown Server ...")
+	//ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+	//defer cancel()
+	//if err := s.Shutdown(ctx); err != nil {
+	//	logging.Fatal("Server Shutdown:", err)
+	//}
+	//logging.Info("Server exiting")
 }
